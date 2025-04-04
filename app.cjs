c8860e04484170a6bc4e798844313f665fa6e404
User <yuko.brandon13@gmail.com>
2025-04-04 18:50:19 +0500

    test

--- app.cjs
+++ app.cjs
@@ -38,8 +38,5 @@ app.post("/AI",async(req,res) =>{
 
 })
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
 module.exports = app
