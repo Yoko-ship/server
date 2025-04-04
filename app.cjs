7b95df1feda7bcf462ac9be3b8eb61ea8f89e3dd
User <yuko.brandon13@gmail.com>
2025-04-04 18:40:37 +0500

    test

--- app.cjs
+++ app.cjs
@@ -38,4 +38,8 @@ const ai =  new googleGenai.GoogleGenAI({apiKey:apiKey})
 
 // })
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 module.exports = app
